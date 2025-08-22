f23810c6a7f57460090f0d93026d7c597f551f8c
Saksham Verma <vermasaksham69@gmail.com>
2025-08-22 18:03:40 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -17,7 +17,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash",
   });
   
   const generationConfig = {
